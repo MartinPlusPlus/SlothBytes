a1b1969b955a0ffdfc4aca63468e2ed6cd6eb427
Martin Eldredge <martin.eldredge1@gmail.com>
2024-08-27 19:19:41 -0600

    changed function to return a string instead of print

--- lovesme.py
+++ lovesme.py
@@ -5,20 +5,22 @@ def loves_me(n: int):
     if n < 1:
         raise ValueError("0 and negative are invalid inputs for loves_me()")
     elif n == 1:
-        print("LOVES ME")
-        return
+        return "LOVES ME"
     
+    s = ""
     if n % 2 == 1:
         for _ in range(math.floor(n/2)):
-            print("Loves me, Loves me not, ", end="")
-        print("LOVES ME")
+            s += "Loves me, Loves me not, "
+        s += "LOVES ME"
     else:
         for _ in range(math.floor(n/2 - 1)):
-            print("Loves me, Loves me not, ", end="")
-        print("Loves me, LOVES ME NOT")
+            s += "Loves me, Loves me not, "
+        s += "Loves me, LOVES ME NOT"
+
+    return s
 
 
 if __name__ == "__main__":
     argv = sys.argv
     times = int(argv[1])
-    loves_me(times)
+    print(loves_me(times))
